94e2a215a152283b9c0e45efe89e1b28a58adc05
Facundo Arambillet <Facundoarambillet@gmail.com>
2023-07-20 22:43:25 -0300

    Test addition with JUnit

--- src/test/java/com/app/TestJava/services/ApiMarvelServiceTest.java
+++ src/test/java/com/app/TestJava/services/ApiMarvelServiceTest.java
@@ -1,25 +1,23 @@
 package com.app.TestJava.services;
 
-
-import com.app.TestJava.models.MarvelCharacter;
 import static org.junit.jupiter.api.Assertions.*;
+import com.app.TestJava.models.MarvelCharacter;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
-
+@SpringBootTest
 public class ApiMarvelServiceTest {
 
     @Autowired
     private ApiMarvelService apiMarvelService;
-
     @DisplayName("Test for get all")
     @Test
     public void getAll() {
 
-
-        List<MarvelCharacter> characters = apiMarvelService.getAll();
+        List<MarvelCharacter> characters = this.apiMarvelService.getAll();
 
         assertNotNull(characters, "The list of characters should not be null");
 
@@ -29,9 +27,9 @@ public class ApiMarvelServiceTest {
             assertTrue(character instanceof MarvelCharacter, "Each element in the list should be an instance of MarvelCharacter");
         }
     }
-
+    @Test
     public void getById() {
-        MarvelCharacter character = apiMarvelService.getById(1011334);
+        MarvelCharacter character = this.apiMarvelService.getById(1011334);
 
         assertNotNull(character,"The character should not be null");
         assertTrue(character.getId() > 0, "The character ID should be greather than 0");
