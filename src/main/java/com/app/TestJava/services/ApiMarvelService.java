7e80a3a19fa0845768e091bc7e446a80a0b7d6f2
Facundo Arambillet <Facundoarambillet@gmail.com>
2023-08-03 22:47:16 -0300

    Creation of endpoint for paging

--- src/main/java/com/app/TestJava/services/ApiMarvelService.java
+++ src/main/java/com/app/TestJava/services/ApiMarvelService.java
@@ -31,7 +31,28 @@ public class ApiMarvelService {
             RestTemplate restTemplate = new RestTemplate();
             timestamp = String.valueOf(Instant.now().toEpochMilli()).substring(0,3);
             hash = MarvelAuthentication.generateHash(this.timestamp,this.privateKey,this.publicKey);
-            String url = this.baseUrl + "?ts=" + this.timestamp + "&apikey=" + this.publicKey + "&hash=" + this.hash;
+            //Agregue el limit para que me traiga 100 en vez de los 20 que trae por default
+            String url = this.baseUrl + "?limit=100" + "&ts=" + this.timestamp + "&apikey=" + this.publicKey + "&hash=" + this.hash;
+            MarvelApi reponse = restTemplate.getForEntity(url, MarvelApi.class).getBody();
+            List<MarvelCharacter> characters = new ArrayList<>();
+            for(MarvelCharacter character : reponse.getData().getResults()) {
+                characters.add(character);
+            }
+            return characters;
+
+        }
+        catch (HttpClientErrorException e) {
+            System.out.println(e);
+            return null;
+        }
+    }
+
+    public List<MarvelCharacter> getByLimit(int limit, int offset) {
+        try {
+            RestTemplate restTemplate = new RestTemplate();
+            timestamp = String.valueOf(Instant.now().toEpochMilli()).substring(0,3);
+            hash = MarvelAuthentication.generateHash(this.timestamp,this.privateKey,this.publicKey);
+            String url = this.baseUrl + "?limit=" + limit + "&offset=" + offset  + "&ts=" + this.timestamp + "&apikey=" + this.publicKey + "&hash=" + this.hash;
             MarvelApi reponse = restTemplate.getForEntity(url, MarvelApi.class).getBody();
             List<MarvelCharacter> characters = new ArrayList<>();
             for(MarvelCharacter character : reponse.getData().getResults()) {
